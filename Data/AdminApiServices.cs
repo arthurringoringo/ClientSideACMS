4f67923b2a1f88f45dde063ef9201bc187ae10a8
Arthur Ringoringo <42938790+arthurringoringo@users.noreply.github.com>
2021-04-21 00:36:44 +0700

    AdminModule

---
+++ Data/AdminApiServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientSideACMS.Data
+{
+    public class AdminApiServices
+    {
+        private HttpClient client = new HttpClient();
+
+        public AdminApiServices(HttpClient Client)
+        {
+            client = Client;
+        }
+    }
+}
